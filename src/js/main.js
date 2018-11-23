fcd8fac0145dc4f4634c2f95b2528cf9dca7e838
George Letsos <it1017@uom.edu.gr>
2018-11-23 19:41:57 +0200

    Changed principles to terms

--- src/js/main.js
+++ src/js/main.js
@@ -24,13 +24,13 @@ for (var li of tableOfContentsUl) {
 var snippets = []; // eslint-disable-line
 
 /*
- * Principles - Tippy.js
+ * Terms - Tippy.js
  */
 tippy.setDefaults({ // eslint-disable-line
   arrow: true
 });
 
-var principles = {
+var terms = {
   parallelize:
     "The parallelize principle means taking advantage of modern CPU capabilities to compute results in less time. This is now possible in any modern browser or NodeJS environment. In the browser, we can achieve true parallelism using web workers. In Node, we can achieve true parallelism by spawning new processes.",
   synchronize:
@@ -39,6 +39,6 @@ var principles = {
     "The conserve principle is about saving on compute and memory resources. This is done by using lazy evaluation techniques. The name lazy stems from the idea that we don't actually compute a new value until we're sure we actually need it. "
 };
 
-tippy("[data-principle='parallelize']", { content: principles.parallelize }); // eslint-disable-line
-tippy("[data-principle='synchronize']", { content: principles.synchronize }); // eslint-disable-line
-tippy("[data-principle='conserve']", { content: principles.conserve }); // eslint-disable-line
+tippy("[data-term='parallelize']", { content: terms.parallelize }); // eslint-disable-line
+tippy("[data-term='synchronize']", { content: terms.synchronize }); // eslint-disable-line
+tippy("[data-term='conserve']", { content: terms.conserve }); // eslint-disable-line
